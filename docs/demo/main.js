38a6fcbfe33995daf35f44a48f68bb7a135641a0
MasatoMakino <unagiinu.g@gmail.com>
2018-12-09 15:11:39 +0900

    キーボードで描画モードを切り替えられるように修正。

--- docs/demo/main.js
+++ docs/demo/main.js
@@ -1,4 +1,7 @@
+import { DrawableBitmap, DrawingMode } from "../../bin/DrawableBitmap";
+
 let stage;
+let bitmap;
 
 const onDomContentsLoaded = () => {
   //ステージ更新処理
@@ -10,10 +13,48 @@ const onDomContentsLoaded = () => {
   const canvas = document.getElementById("appCanvas");
   stage = new createjs.Stage(canvas);
   stage.enableMouseOver();
+  createjs.Touch.enable(stage);
   console.log(stage._mouseOverIntervalID);
 
   createjs.Ticker.on("tick", updateStage);
+  testBitmap();
+  startDrawing();
 };
+
+const testBitmap = () => {
+  bitmap = new DrawableBitmap(640, 480);
+  stage.addChild(bitmap);
+};
+
+const startDrawing = () => {
+  bitmap.startDrawing({
+    mode: DrawingMode.pen,
+    color: "#F0F",
+    width: 1.0
+  });
+};
+
+const startEraser = () => {
+  bitmap.startDrawing({
+    mode: DrawingMode.eraser,
+    width: 8.0
+  });
+};
+
+document.addEventListener("keydown", e => {
+  console.log(e.key);
+  switch (e.key) {
+    case "e":
+      startEraser();
+      break;
+    case "d":
+      startDrawing();
+      break;
+    case "c":
+      bitmap.clear();
+      break;
+  }
+});
 
 /**
  * DOMContentLoaded以降に初期化処理を実行する
