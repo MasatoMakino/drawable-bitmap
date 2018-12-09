dfec0b89f6080bd687d83145d3578a714ab20462
MasatoMakino <unagiinu.g@gmail.com>
2018-12-09 17:04:13 +0900

    コメント整備。

--- src/DrawableBitmap.ts
+++ src/DrawableBitmap.ts
@@ -7,6 +7,7 @@ export class DrawableBitmap extends Bitmap {
   private option?: DrawingOption;
   private isDrawing: boolean = false;
   private points: Map<number, Point>;
+
   /**
    * コンストラクタ
    * @param {number} w
@@ -21,6 +22,10 @@ export class DrawableBitmap extends Bitmap {
     this.initHitArea();
   }
 
+  /**
+   * ヒットエリアを初期化する。
+   * CreateJSでは透明なピクセルはマウスイベントにヒットしないため、描画エリアの矩形を明示的にヒットエリアに指定する。
+   */
   private initHitArea(): void {
     const area = new Shape();
     area.graphics
@@ -29,12 +34,18 @@ export class DrawableBitmap extends Bitmap {
       .endFill();
     this.hitArea = area;
   }
+
   /**
    * 描画状態を画像から復元する
    * @param {string} url
    */
   public restoreImage(url: string): void {}
 
+  /**
+   * 描画モードを更新する。
+   * 描画モードオプションは指定のある値だけが利用され、未指定のものは現状値が引き継がれる。
+   * @param {DrawingOption} option
+   */
   public changeMode(option: DrawingOption): void {
     this.option = option;
 
@@ -73,29 +84,26 @@ export class DrawableBitmap extends Bitmap {
     this.removeEventListener("pressup", this.onFinishStroke);
   }
 
+  /**
+   * ストローク処理が開始された際の処理。
+   * ストローク座標を記録する。
+   * @param {createjs.MouseEvent} e
+   */
   private onStartStroke = (e: MouseEvent) => {
     this.points.set(e.pointerID, new Point(e.localX, e.localY));
   };
 
+  /**
+   * ストローク中の処理
+   * @param {createjs.MouseEvent} e
+   */
   private onStroke = (e: MouseEvent) => {
     const point = this.points.get(e.pointerID);
     if (point == null) return;
 
+    this.updateStrokeStyle();
+
     const ctx = this.ctx;
-
-    switch (this.option.mode) {
-      case DrawingMode.pen:
-        ctx.globalCompositeOperation = "source-over";
-        ctx.strokeStyle = this.option.color;
-        break;
-      case DrawingMode.eraser:
-        ctx.globalCompositeOperation = "destination-out";
-        break;
-    }
-    ctx.lineWidth = this.option.width;
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-
     ctx.beginPath();
     ctx.moveTo(point.x, point.y);
     ctx.lineTo(e.localX, e.localY);
@@ -105,12 +113,37 @@ export class DrawableBitmap extends Bitmap {
     this.points.set(e.pointerID, new Point(e.localX, e.localY));
   };
 
+  /**
+   * 2dコンテクストのストロークスタイルを更新する。
+   * onStroke関数の内部処理。
+   */
+  private updateStrokeStyle(): void {
+    const ctx = this.ctx;
+    switch (this.option.mode) {
+      case DrawingMode.pen:
+        ctx.globalCompositeOperation = "source-over";
+        break;
+      case DrawingMode.eraser:
+        ctx.globalCompositeOperation = "destination-out";
+        break;
+    }
+    ctx.strokeStyle = this.option.color;
+    ctx.lineWidth = this.option.width;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+  }
+
+  /**
+   * ストロークが終了した際の処理。
+   * 座標マップから座標値を削除する。
+   * @param {createjs.MouseEvent} e
+   */
   private onFinishStroke = (e: MouseEvent) => {
     this.points.delete(e.pointerID);
   };
 
   /**
-   * 画像をクリアする。全ての画素はrgba( 0, 0, 0, 0.0)となる。
+   * 画像をクリアする。全ての画素はrgba(0, 0, 0, 0.0)となる。
    */
   public clear(): void {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
