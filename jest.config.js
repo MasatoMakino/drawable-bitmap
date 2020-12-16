fbb61740ce3f36afb4fa8f37eac159d6306ebb22
MasatoMakino <unagiinu.g@gmail.com>
2020-12-16 15:06:48 +0900

    remove : travis setting

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  collectCoverageFrom: [
-    "src/**/*.{js,ts}",
-    "!<rootDir>/__test__/",
-    "!<rootDir>/docs/api/",
-    "!<rootDir>/task/"
-  ]
-};
